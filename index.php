c85c4de13960ef00d0e5a298d08ada5d355d0087
shahanas123 <mca545@rajagiri.edu>
2021-03-19 20:49:31 +0530

    push

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="http://gfdrr.github.io/innovation-lab-brand-guidelines/images/01-brand-guidelines/01-07-download-resources/1-7-e5.png" class="img-thumbnail" alt="...">Kart</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="electronics.php">Electronics</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="groceries.php">Groceries</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="fashion.php">Fashion</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Login
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Buyer</a></li>
+              <li><a class="dropdown-item" href="#">Seller</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Admin</a></li>
+            </ul>
+          </li>
+         
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+<body>
+    <div class="container">
+        <div class="row">
+            <img height="300px" src="https://ak.picdn.net/shutterstock/videos/32487439/thumb/6.jpg" class="d-block w-100" alt="...">
+
+        </div>
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="card" style="width: 18rem;">
+                    <img height="250px"src="https://www.orissapost.com/wp-content/uploads/2018/12/news12_5.jpg" class="card-img-top" alt="...">
+                    
+                    <div class="card-body">
+                      
+                      <a href="electronics.php" class="card-link">Electronics</a>
+                    </div>
+                </div>
+                  
+            </div>
+            <div class="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="card" style="width: 18rem;">
+                    <img height="250px"src="https://image.shutterstock.com/image-photo/paper-bag-fresh-vegetables-bottle-260nw-1410320450.jpg" class="card-img-top" alt="...">
+                    
+                    <div class="card-body">
+                      
+                      <a href="groceries.php" class="card-link">Groceries</a>
+                    </div>
+                </div>
+                  
+            </div>
+           
+            <div class="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="card" style="width: 18rem;">
+                    <img height="250px" src="https://image.shutterstock.com/image-photo/fashion-young-woman-trendy-spring-260nw-571220725.jpg" class="card-img-top" alt="...">
+                    
+                    <div class="card-body">
+                      
+                      <a href="fashion.php" class="card-link">fashion</a>
+                    </div>
+                </div>
+            
+        
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
